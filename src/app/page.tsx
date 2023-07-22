7e6914e86cde2e50be820cd64a4949a3e1f8b31b
nofamex <nsiren517@gmail.com>
2023-07-23 00:47:57 +0700

    project list

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
-import Navbar from "@/components/container/navbar"
+import Navbar from "@/components/container/navbar";
 import Welcome from "@/components/container/welcome";
+import ProjectList from "@/components/container/welcome/projectList";
 
 export default function Home() {
   return (
     <div className="w-full h-full">
       <Navbar />
       <Welcome />
+      <ProjectList />
     </div>
   );
 }
